282297e1e3d5a4d9bf9bf988bb2f645ae01e8c9a
tbs99 <timsregisters@gmail.com>
2020-02-23 18:02:51 +0300

    initial commit

--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from flask import render_template, request, session, flash, redirect
 import random
 from flask_wtf.csrf import CSRFProtect
 import datetime
-from flask_login import login_user, logout_user
 from flask_login import LoginManager, login_required, current_user, login_user, logout_user
 
 from app import app
